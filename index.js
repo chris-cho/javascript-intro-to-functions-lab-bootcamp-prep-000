9480139caf1693a7e3a49b0339311992ce622f94
Hyun sang Cho <hyunsangc@gmail.com>
2018-08-18 03:39:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,11 @@ function logWhisper(string){
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string){
-  if(string == string.toLowerCase()){
+  if(string === string.toLowerCase()){
     console.log("I can't hear you!")
   }
+  else (string === "I love u, Grandma."){
+    console.log("I love you, too.")
+  }
+  else (string ===)
 }
